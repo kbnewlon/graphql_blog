450d813ca8e2011dd1c30c621b1e090d715e603c
Kayla Newlon <kayla.b.newlon@gmail.com>
2022-02-28 17:34:33 -0800

    successful map

---
+++ pages/index.js
@@ -0,0 +1,25 @@
+import Head from 'next/head'
+
+const posts = [
+  { title: 'React Testing', excerpt: 'Learn React Testing' },
+  { title: 'React with Tailwind', excerpt: 'Learn React with Tailwind' },
+
+]
+export default function Home() {
+  return (
+    <div className='container mx-auto px-10 mb-8'>
+      <Head>
+        <title>GraphCMS Blog</title>
+        <link rel='icon' href='/favicon' />
+      </Head>
+
+      <div className='grid grid-cols-lg:grid-cols-12 gap-12'>
+        {posts.map((post, index) => (
+          <div>{post.title}
+            {post.excerpt}</div>
+        ))}
+      </div>
+
+    </div>
+  )
+}
