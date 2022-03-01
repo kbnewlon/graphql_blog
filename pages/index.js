95b4c50ff1e8b760af59fd7d5e913b2a074c2d10
Kayla Newlon <kayla.b.newlon@gmail.com>
2022-02-28 17:42:21 -0800

    add container

--- pages/index.js
+++ pages/index.js
@@ -7,16 +7,18 @@ const posts = [
 ]
 export default function Home() {
   return (
-    <div className='container mx-auto px-10 mb-8'>
+    <div className='container mx-auto px-10 mb-8 bg-grey-300'>
       <Head>
         <title>GraphCMS Blog</title>
         <link rel='icon' href='/favicon' />
       </Head>
 
-      <div className='grid grid-cols-lg:grid-cols-12 gap-12'>
+      <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
         {posts.map((post, index) => (
-          <div>{post.title}
-            {post.excerpt}</div>
+          <div>
+            {post.title}
+            {post.excerpt}
+            </div>
         ))}
       </div>
 
