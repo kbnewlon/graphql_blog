6f05ed17b54c3b1b9ca4be06cfe12010f0b651f0
Kayla Newlon <kayla.b.newlon@gmail.com>
2022-03-02 09:55:29 -0800

    setting up components

--- pages/index.js
+++ pages/index.js
@@ -7,20 +7,28 @@ const posts = [
 ]
 export default function Home() {
   return (
-    <div className='container mx-auto px-10 mb-8 bg-grey-300'>
+    <div className='container mx-auto px-10 mb-8'>
       <Head>
         <title>GraphCMS Blog</title>
         <link rel='icon' href='/favicon' />
       </Head>
 
       <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
-        {posts.map((post, index) => (
-          <div>
-            {post.title}
-            {post.excerpt}
+        <div className='lg:col-span-8 col-span-1'>
+          {posts.map((post, index) => (
+            <div key={post.index}>
+              {post.title}
+              {post.excerpt}
             </div>
-        ))}
+          ))}
+        </div>
+        <div className='lg:col-span-4 col-span-1'>
+          <div className='lg:sticky relative top-8'>
+
+          </div>
+        </div>
       </div>
+
 
     </div>
   )
