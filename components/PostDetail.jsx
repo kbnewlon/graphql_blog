f0dc9717765f0b79debe65950ff11ab01b893716
Kayla Newlon <kayla.b.newlon@gmail.com>
2022-03-07 15:21:15 -0800

    rendering content

--- components/PostDetail.jsx
+++ components/PostDetail.jsx
@@ -6,6 +6,46 @@ import { graphCMSImageLoader } from '../util'
 
 
 const PostDetail = ({ post }) => {
+    const getContentFragment = (index, text, obj, type) => {
+        let modifiedText = text;
+
+        if (obj) {
+            if (obj.bold) {
+                modifiedText = (<b key={index}>{text}</b>);
+            }
+
+            if (obj.italic) {
+                modifiedText = (<em key={index}>{text}</em>);
+            }
+
+            if (obj.underline) {
+                modifiedText = (<u key={index}>{text}</u>);
+            }
+        }
+
+        switch (type) {
+            case 'heading-three':
+                return <h3 key={index} className="text-xl font-semibold mb-4">{modifiedText.map((item, i) => <React.Fragment key={i}>{item}</React.Fragment>)}</h3>;
+            case 'paragraph':
+                return <p key={index} className="mb-8">{modifiedText.map((item, i) => <React.Fragment key={i}>{item}</React.Fragment>)}</p>;
+            case 'heading-four':
+                return <h4 key={index} className="text-md font-semibold mb-4">{modifiedText.map((item, i) => <React.Fragment key={i}>{item}</React.Fragment>)}</h4>;
+            case 'image':
+                return (
+                    <img
+                        key={index}
+                        alt={obj.title}
+                        height={obj.height}
+                        width={obj.width}
+                        src={obj.src}
+                    />
+                );
+            default:
+                return modifiedText;
+        }
+    };
+
+
     return (
         <div className='bg-white shadow-lg rounded-lg lg:p-8 pb-12 mb-8'>
             <div className='relative overflow-hidden shadow-md mb-6'>
@@ -19,7 +59,7 @@ const PostDetail = ({ post }) => {
             <div className='px-4 lg:px-0'>
                 <div className='flex items-center mb-8 w-full'>
 
-                    <div className="flex items-center justify-center mb-4 lg:mb-0 w-full lg:w-auto mr-8 items-center">
+                    <div className="flex items-center mb-4 lg:mb-0 w-full lg:w-auto mr-8 items-center">
                         <Image
                             unoptimized
                             loader={graphCMSImageLoader}
@@ -38,6 +78,13 @@ const PostDetail = ({ post }) => {
                         <span className="align-middle">{moment(post.createdAt).format('MMM DD, YYYY')}</span>
                     </div>
                 </div>
+
+                <h1 className="mb-8 text-3xl font-semibold">{post.title}</h1>
+                {post.content.raw.children.map((typeObj, index) => {
+                    const children = typeObj.children.map((item, itemindex) => getContentFragment(itemindex, item.text, item));
+
+                    return getContentFragment(index, children, typeObj, typeObj.type);
+                })}
             </div>
         </div>
     )
