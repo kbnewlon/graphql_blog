d5c49e6ceed72b7132463507996d75891d75704a
Kayla Newlon <kayla.b.newlon@gmail.com>
2022-03-07 13:37:25 -0800

    services

--- components/PostWidget.jsx
+++ components/PostWidget.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import moment from  'moment'
 import Link from 'next/link'
 
+import { getRecentPost } from '../services'
 
-const PostWidget = () => {
+const PostWidget = ({ categories, slug }) => {
+  const [relatedPosts, setRelatedPosts] = useState([])
+
+
+  useEffect(() => {
+    if(slug){
+      getSimilarPosts(category, slug)
+      .then((result) => setRelatedPosts(result))
+    } else{
+      getRecentPost()
+      .then((result) => getRecentPost(result))
+    }
+  }, [])
+  
   return (
     <div>PostWidget</div>
   )
