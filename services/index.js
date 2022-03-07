82cd342f4db6814662033384e3fbd7dc43afb450
Kayla Newlon <kayla.b.newlon@gmail.com>
2022-03-07 10:48:05 -0800

    changed gql to authors fixed errors

--- services/index.js
+++ services/index.js
@@ -2,13 +2,13 @@ import { request, gql } from 'graphql-request'
 const graphqlAPI = process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT
 
 export const getPosts = async () => {
-    const query = gql`
+  const query = gql`
     query MyQuery {
       postsConnection {
         edges {
           cursor
           node {
-            author {
+            authors {
               bio
               name
               id
@@ -31,9 +31,9 @@ export const getPosts = async () => {
         }
       }
     }
-  ` 
+  `
 
   const result = await request(graphqlAPI, query)
 
-  return result.postsConnection.edges;
+  return result.postsConnection.edges
 }
