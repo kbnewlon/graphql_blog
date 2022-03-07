b7ec4984d626cd835f6d65c919e6e7ccb32e72e3
Kayla Newlon <kayla.b.newlon@gmail.com>
2022-03-07 14:27:17 -0800

    header key props error fixed

--- components/Categories.jsx
+++ components/Categories.jsx
@@ -10,6 +10,7 @@ const Categories = () => {
     getCategories()
       .then((newCategories) => setCategories(newCategories))
   }, [])
+  
   return (
     <div className="bg-white shadow-lg rounded-lg p-8 pb-12 mb-8">
       <h3 className="text-xl mb-8 font-semibold border-b pb-4">
