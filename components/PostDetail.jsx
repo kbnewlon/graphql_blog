0ddd843c52c5132a5346b3d26ef75b8f8d49b937
Kayla Newlon <kayla.b.newlon@gmail.com>
2022-03-07 17:29:05 -0800

    author published on bottom of postdetails

--- components/PostDetail.jsx
+++ components/PostDetail.jsx
@@ -6,6 +6,7 @@ import { graphCMSImageLoader } from '../util'
 
 
 const PostDetail = ({ post }) => {
+
     const getContentFragment = (index, text, obj, type) => {
         let modifiedText = text;
 
@@ -47,45 +48,65 @@ const PostDetail = ({ post }) => {
 
 
     return (
-        <div className='bg-white shadow-lg rounded-lg lg:p-8 pb-12 mb-8'>
-            <div className='relative overflow-hidden shadow-md mb-6'>
-                <img
-                    src={post.featuredImage.url}
-                    alt={post.title}
-                    className='object-top h-full w-full rounded-t-lg'
-                />
+        <div>
+            <div className='bg-white shadow-lg rounded-lg lg:p-8 pb-12 mb-8'>
+                <div className='relative overflow-hidden shadow-md mb-6'>
+                    <img
+                        src={post.featuredImage.url}
+                        alt={post.title}
+                        className='object-top h-full w-full rounded-t-lg'
+                    />
 
-            </div>
-            <div className='px-4 lg:px-0'>
-                <div className='flex items-center mb-8 w-full'>
-
-                    <div className="flex items-center mb-4 lg:mb-0 w-full lg:w-auto mr-8 items-center">
-                        <Image
-                            unoptimized
-                            loader={graphCMSImageLoader}
-                            alt={post.authors[0].name}
-                            height="30px"
-                            width="30px"
-                            className="align-middle rounded-full"
-                            src={post.authors[0].photo.url}
-                        />
-                        <p className="inline align-middle text-gray-700 ml-2 font-medium text-lg">{post.authors[0].name}</p>
-                    </div>
-                    <div className="font-medium text-gray-700">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 inline mr-2 text-pink-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
-                        </svg>
-                        <span className="align-middle">{moment(post.createdAt).format('MMM DD, YYYY')}</span>
-                    </div>
                 </div>
+                <div className='px-4 lg:px-0'>
+                    <div className='flex items-center mb-8 w-full'>
 
-                <h1 className="mb-8 text-3xl font-semibold">{post.title}</h1>
-                {post.content.raw.children.map((typeObj, index) => {
-                    const children = typeObj.children.map((item, itemindex) => getContentFragment(itemindex, item.text, item));
+                        <div className="flex items-center mb-4 lg:mb-0 w-full lg:w-auto mr-8 items-center">
+                            <Image
+                                unoptimized
+                                loader={graphCMSImageLoader}
+                                alt={post.authors[0].name}
+                                height="30px"
+                                width="30px"
+                                className="align-middle rounded-full"
+                                src={post.authors[0].photo.url}
+                            />
+                            <p className="inline align-middle text-gray-700 ml-2 font-medium text-lg">{post.authors[0].name}</p>
+                        </div>
+                        <div className="font-medium text-gray-700">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 inline mr-2 text-pink-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
+                            </svg>
+                            <span className="align-middle">{moment(post.createdAt).format('MMM DD, YYYY')}</span>
+                        </div>
+                    </div>
 
-                    return getContentFragment(index, children, typeObj, typeObj.type);
-                })}
+                    <h1 className="mb-8 text-3xl font-semibold">{post.title}</h1>
+                    {post.content.raw.children.map((typeObj, index) => {
+                        const children = typeObj.children.map((item, itemindex) => getContentFragment(itemindex, item.text, item));
+
+                        return getContentFragment(index, children, typeObj, typeObj.type);
+                    })}
+                </div>
             </div>
+
+            <div className="text-center mt-20 mb-9 p-12 relative rounded-lg bg-black bg-opacity-20">
+                <div className="absolute left-0 right-0 -top-14">
+                    <Image
+                        unoptimized
+                        loader={graphCMSImageLoader}
+                        alt={post.authors[0].name}
+                        height="100px"
+                        width="100px"
+                        className="align-middle rounded-full"
+                        src={post.authors[0].photo.url}
+                    />
+                </div>
+                <h3 className="text-white mt-4 mb-4 text-xl font-bold">{post.authors[0].name}</h3>
+                <p className="text-white text-ls">{post.authors[0].bio}</p>
+
+            </div>
+
         </div>
     )
 }
