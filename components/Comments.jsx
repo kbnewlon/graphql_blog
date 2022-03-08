93960fabd7f39e8a206230266e7170b83d33d53d
Kayla Newlon <kayla.b.newlon@gmail.com>
2022-03-08 11:57:59 -0800

    getComments

--- components/Comments.jsx
+++ components/Comments.jsx
@@ -1,10 +1,23 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import moment from 'moment'
+import parse from 'html-react-parser'
 
-const Comments = () => {
+import { getComments } from '../services'
+
+const Comments = ({ slug }) => {
+  const [comments, setComments] = useState([second])
+
+  useEffect(() => {
+    getComments(slug)
+      .then((result) => setComments(result))
+  }, [])
+
+
   return (
-    <div>
-        <h1>Comments</h1>
-    </div>
+    <>
+
+
+    </>
   )
 }
 
